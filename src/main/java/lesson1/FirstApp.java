7880f8bfc4535315ee4ba3ddb71e5d9b340f659c
Artem <letnev2012@gmail.com>
2023-02-25 18:53:33 +0300

    homework1

---
+++ src/main/java/lesson1/FirstApp.java
@@ -0,0 +1,70 @@
+package lesson1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        drawCat();
+//        System.out.println("Hellow Java!");
+//        int max = 10;
+//        float val = 12;
+//        double doubleValue = 22.2;
+//
+//        int b = 100 + max;
+//        System.out.println("b = " + b + "sss");
+//
+//        int c = (max + b) * 2;
+//        System.out.println("c = " + c);
+//
+//        int a = 12;
+//        a++;
+//        System.out.println(a);
+//        a += 10;
+//        System.out.println(a);
+
+        //boolean value = true;
+       // System.out.println(value);
+
+//        int v1 = 22;
+//        int v2 = 22;
+
+        //boolean resulst = v1 > v2;
+//        System.out.println(resulst);
+
+//        if(v1 >v2) {
+//            System.out.println("v1 > v2");
+//        } else if (v1<v2) {
+//            System.out.println("v1 < v2");
+//
+//        } else {
+//            System.out.println("ravno");
+//        }
+
+    }
+    public static void drawCat(){
+        System.out.println("sad");
+
+    }
+
+    // 2 типа данных
+    // ссылочные
+    // примитивные
+
+    // численные типы
+    // byte  от -128 да 127
+    // short 2(16) от -2(15) ... 2(15) -1
+    // int 2(32)
+    // long 2(64)
+    //
+    // числа с плавающей точкой
+    //float 2(32)
+    //double 2(64)
+
+
+    // boolean
+    // значения только true false
+
+
+    // символьный тип
+    //char (ASCII/Unicod
+
+
+}
